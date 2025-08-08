20fecbfcf1522a6a468482bf3b754f7a54eb1405
Adam Eury <euryadam@gmail.com>
2025-08-08 11:26:18 -0400

    include module name in formatted test result (#2)
    * include module name in formatted test result
    
    * Rename variable
    
    * refactor parse_line regex to named capture groups
    
    * simplify line parsing by removing regex. update prettiify to handle multiple underscores in test names.
    
    * apply humanization to the sentence part of a test name with _fn_
    
    * use unqualified names of well-known traits
    
    * replace generic parameters with impl trait
    
    * add module example to the readme
    
    * Update prettify_module
    
    Co-authored-by: John Arundel <john@bitfieldconsulting.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,7 @@
-//! A Cargo subcommand for printing your Rust test names as sentences.
-//!
-//! Also contains functions to parse the (human-readable) output of `cargo test`,
-//! and to format test names as sentences.
-//!
-//! Further reading and context: [Test names should be
-//! sentences](https://bitfieldconsulting.com/posts/test-names).
-use anyhow::Context;
+#![doc = include_str!("../README.md")]
+use anyhow::{anyhow, Context};
 use colored::Colorize;
-use regex::Regex;
-use std::{process::Command, sync::LazyLock};
+use std::{fmt::Display, process::Command, str::FromStr};
 
 #[must_use]
 /// Runs `cargo test` with any supplied extra arguments, and returns the
@@ -38,28 +31,25 @@ pub fn parse_test_results(test_output: &str) -> Vec<TestResult> {
     test_output.lines().filter_map(parse_line).collect()
 }
 
-static MODULE_PREFIX: LazyLock<Regex> = LazyLock::new(|| Regex::new(".*::").unwrap());
-
 /// Parses a line from the standard output of `cargo test`.
 ///
 /// If the line represents the result of a test, returns `Some(TestResult)`,
 /// otherwise returns `None`.
-pub fn parse_line<S: AsRef<str>>(line: S) -> Option<TestResult> {
+pub fn parse_line(line: impl AsRef<str>) -> Option<TestResult> {
     let line = line.as_ref().strip_prefix("test ")?;
     if line.starts_with("result") || line.contains("(line ") {
         return None;
     }
-    let line = MODULE_PREFIX.replace(line, "");
-    let splits: Vec<_> = line.split(" ... ").collect();
-    let (name, result) = (splits[0], splits[1]);
+
+    let (test, status) = line.split_once(" ... ")?;
+    let (module, name) = match test.rsplit_once("::") {
+        Some((module, name)) => (prettify_module(module), name),
+        None => (None, test),
+    };
     Some(TestResult {
+        module,
         name: prettify(name),
-        status: match result {
-            "ok" => Status::Pass,
-            "FAILED" => Status::Fail,
-            "ignored" => Status::Ignored,
-            _ => todo!("unhandled test status {:?}", result),
-        },
+        status: status.parse().ok()?,
     })
 }
 
@@ -77,33 +67,52 @@ pub fn parse_line<S: AsRef<str>>(line: S) -> Option<TestResult> {
 /// ```text
 /// parse_line parses a line
 /// ```
-pub fn prettify<S: AsRef<str>>(input: S) -> String {
-    let mut output = String::new();
+pub fn prettify(input: impl AsRef<str>) -> String {
     if let Some((fn_name, sentence)) = input.as_ref().split_once("_fn_") {
-        output.push_str(fn_name);
-        output.push(' ');
-        output.push_str(sentence.replace('_', " ").as_ref());
+        format!("{} {}", fn_name, humanize(sentence))
     } else {
-        output.push_str(input.as_ref().replace('_', " ").as_ref());
+        humanize(input)
     }
-    output
+}
+
+fn humanize(input: impl AsRef<str>) -> String {
+    input
+        .as_ref()
+        .replace('_', " ")
+        .split_whitespace()
+        .collect::<Vec<&str>>()
+        .join(" ")
+}
+
+fn prettify_module(module: &str) -> Option<String> {
+    let mut parts = module.split("::").collect::<Vec<_>>();
+    parts.pop_if(|&mut s| s == "tests" || s == "test");
+    if parts.is_empty() {
+        return None;
+    }
+    Some(parts.join("::"))
 }
 
 #[derive(Debug, PartialEq)]
 /// The (prettified) name and pass/fail status of a given test.
 pub struct TestResult {
+    pub module: Option<String>,
     pub name: String,
     pub status: Status,
 }
 
-impl std::fmt::Display for TestResult {
+impl Display for TestResult {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let status = match self.status {
-            Status::Pass => "✔".bright_green(),
-            Status::Fail => "x".bright_red(),
-            Status::Ignored => "?".bright_yellow(),
-        };
-        write!(f, " {status} {}", self.name)
+        match &self.module {
+            Some(module) => write!(
+                f,
+                "{} {} – {}",
+                self.status,
+                module.bright_blue(),
+                self.name
+            ),
+            None => write!(f, "{} {}", self.status, self.name),
+        }
     }
 }
 
@@ -113,6 +122,30 @@ pub enum Status {
     Pass,
     Fail,
     Ignored,
+}
+
+impl FromStr for Status {
+    type Err = anyhow::Error;
+
+    fn from_str(status: &str) -> Result<Self, Self::Err> {
+        match status {
+            "ok" => Ok(Status::Pass),
+            "FAILED" => Ok(Status::Fail),
+            "ignored" => Ok(Status::Ignored),
+            _ => Err(anyhow!("unhandled test status {status:?}")),
+        }
+    }
+}
+
+impl Display for Status {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let status = match self {
+            Status::Pass => "✔".bright_green(),
+            Status::Fail => "x".bright_red(),
+            Status::Ignored => "?".bright_yellow(),
+        };
+        write!(f, "{status}")
+    }
 }
 
 #[cfg(test)]
@@ -142,6 +175,14 @@ mod tests {
                 input: "parse_line_fn_does_stuff",
                 want: "parse_line does stuff".into(),
             },
+            Case {
+                input: "prettify__handles_multiple_underscores",
+                want: "prettify handles multiple underscores".into(),
+            },
+            Case {
+                input: "prettify_fn__handles_multiple_underscores",
+                want: "prettify handles multiple underscores".into(),
+            },
         ]);
         for case in cases {
             assert_eq!(case.want, prettify(case.input));
@@ -162,13 +203,23 @@ mod tests {
             Case {
                 line: "test foo ... ok",
                 want: Some(TestResult {
+                    module: None,
                     name: "foo".into(),
+                    status: Status::Pass,
+                }),
+            },
+            Case {
+                line: "test foo::tests::does_foo_stuff ... ok",
+                want: Some(TestResult {
+                    module: Some("foo".into()),
+                    name: "does foo stuff".into(),
                     status: Status::Pass,
                 }),
             },
             Case {
                 line: "test tests::urls_correctly_extracts_valid_urls ... FAILED",
                 want: Some(TestResult {
+                    module: None,
                     name: "urls correctly extracts valid urls".into(),
                     status: Status::Fail,
                 }),
@@ -176,6 +227,23 @@ mod tests {
             Case {
                 line: "test files::test::files_can_be_sorted_in_descending_order ... ignored",
                 want: Some(TestResult {
+                    module: Some("files".into()),
+                    name: "files can be sorted in descending order".into(),
+                    status: Status::Ignored,
+                }),
+            },
+            Case {
+                line: "test files::test::foo::tests::files_can_be_sorted_in_descending_order ... ignored",
+                want: Some(TestResult {
+                    module: Some("files::test::foo".into()),
+                    name: "files can be sorted in descending order".into(),
+                    status: Status::Ignored,
+                }),
+            },
+            Case {
+                line: "test files::test_foo::files_can_be_sorted_in_descending_order ... ignored",
+                want: Some(TestResult {
+                    module: Some("files::test_foo".into()),
                     name: "files can be sorted in descending order".into(),
                     status: Status::Ignored,
                 }),
@@ -183,6 +251,14 @@ mod tests {
             Case {
                 line: "test src/lib.rs - find_top_n_largest_files (line 17) ... ok",
                 want: None,
+            },
+            Case {
+                line: "test output_format::_concise_expects ... ok",
+                want: Some(TestResult {
+                    module: Some("output_format".into()),
+                    name: "concise expects".into(),
+                    status: Status::Pass,
+                }),
             },
         ]);
         for case in cases {
